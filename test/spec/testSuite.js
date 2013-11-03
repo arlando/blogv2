f9708f88ccace01ca603635520ff037ffff45fb7
Arlando Battle <arlando@Arlandos-MacBook-Pro.local>
2013-11-03 10:01:09 -0500

    readding deleted test files…

--- test/spec/testSuite.js
+++ test/spec/testSuite.js
@@ -3,7 +3,13 @@ define(function() {
 
 	/* return an array of specs to be run */
 	return {
-		specs: ['spec/exampleTest'
+		specs: ['spec/exampleTest',
+			'spec/collections/Tracks.js',
+			'spec/exampleTest.js',
+			'spec/models/Track.js',
+			'spec/routers/BlogRouter.js',
+			'spec/views/collection/Tracks.js',
+			'spec/views/item/Track.js'
 		]
 	};
 });
---
+++ test/spec/views/layout/SpotifyLayout.js
@@ -0,0 +1,26 @@
+
+(function() {
+  'use strict';
+
+  var root = this;
+
+  root.define([
+    'views/layout/SpotifyLayout'
+    ],
+    function( Spotifylayout ) {
+
+      describe('Spotifylayout Layout', function () {
+
+        it('should be an instance of Spotifylayout Layout', function () {
+          var SpotifyLayout = new Spotifylayout();
+          expect( SpotifyLayout ).to.be.an.instanceof( Spotifylayout );
+        });
+
+        it('should have more test written', function(){
+          expect( false ).to.be.ok;
+        });
+      });
+
+    });
+
+}).call( this );
