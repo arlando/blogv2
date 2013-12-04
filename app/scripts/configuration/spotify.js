de1c6c1ef4907ea4459d992d9a61c350d8cf3a0c
Arlando Battle <arlando@1stdibs.com>
2013-12-03 22:09:04 -0500

    dst

--- app/scripts/configuration/spotify.js
+++ app/scripts/configuration/spotify.js
@@ -3,7 +3,7 @@ define({
         {id:1, title: 'Woot1', spotifyuri: '4YrRh7kNgbU2nB4eGIlKeM', artist:'Thing1', playing:true },
         {id:2, title: 'Woot2', spotifyuri: '6NdSZVSwqlNdkfee1byLAv', artist:'Thing2' },
         {id:3, title: 'Woot3', spotifyuri: '0AMMjcRxVNOnQjmdXNgF2e', artist:'Thing3' },
-        {id:4, title: 'Thing4', spotifyuri: '5WACLmxePHgd45hptug9n1', artist:'Thing4' },
+        {id:4, title: 'Woo4', spotifyuri: '5WACLmxePHgd45hptug9n1', artist:'Thing4' },
         {id:5, title: 'Thing5', spotifyuri: '2OCHjMefU4lUWLNz8sKw48', artist:'Thing5' }
     ],
     layout: '.layout-spotify'
